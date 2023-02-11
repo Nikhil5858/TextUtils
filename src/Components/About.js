521a50301f05fd6499cbe8d6ee6644c83dfbc4e9
Nikhil Rathod <123rathodnikhil@gmail.com>
2023-02-11 11:31:10 +0530

    about

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,14 +1,10 @@
 import React, { useState } from "react";
 
 export default function About(props) {
-  const [mystyle, setmystyle] = useState({
-    color: "black",
-    backgroundColor: "white",
-  });
-  const [btntext, setbtntext] = useState("Enable Dark Mode");
+  
   return (
     <div>
-      <div className="container" style={mystyle}>
+      <div className="container" >
       </div>
     </div>
   );
